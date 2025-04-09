f5cc67eff702e0fe287b11de35d4fb18a4999dc9
Nikol Pavlovská <152870535+00Nikol00@users.noreply.github.com>
2025-04-09 12:27:27 +0200

    test_click

--- tests/test_lolcator.py
+++ tests/test_lolcator.py
@@ -11,6 +11,19 @@ def test_page_title():
         page = browser.new_page()
         page.goto(f"file:///{cesta}")
         nadpis_1 = page.locator('h1').first
+        nadpis_text=page.locator('text="Nadpis 1"')
+        div_1 = page.locator('.container')
+        div_2 = page.locator('.container-2')
+        odkaz = page.locator('text="Odkaz"')
+        odkaz_2 = page.locator('a[href="https://www.w3schools.com/"]')
         expect(nadpis_1).to_be_visible()
-
+        expect(nadpis_text).to_be_visible()
+        expect(div_1).to_be_visible()
+        expect(div_2).to_be_visible()
+        expect(nadpis_text).to_have_count(1)
+        odkaz_2.click()
+        expect(page).to_have_url("https://www.w3schools.com/")
+        expect(odkaz).to_have_text("Odkaz")
+        
+        
         browser.close()
